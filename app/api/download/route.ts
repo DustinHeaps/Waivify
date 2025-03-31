98e110467cef278093f05f014fe9db9bdc249015
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2025-03-31 17:25:42 -0400

    ninor fix

--- app/api/download/route.ts
+++ app/api/download/route.ts
@@ -2,42 +2,38 @@
 // import { renderToBuffer } from "@react-pdf/renderer";
 // import { prisma } from "@/lib/prisma";
 
+import { NextResponse } from "next/server";
+
 // import WaiverPDF from "@/components/WaiverPDF";
 // import { format } from "date-fns";
 // import { createElement } from 'react';
 
-// export const runtime = "nodejs"; 
+// export const runtime = "nodejs";
 
-// export async function GET(req: Request) {
-//   const { searchParams } = new URL(req.url);
-//   const waiverId = searchParams.get("waiverId");
+export async function GET(req: Request) {
+  //   const { searchParams } = new URL(req.url);
+  //   const waiverId = searchParams.get("waiverId");
 
-//   if (!waiverId) return new NextResponse("Missing waiverId", { status: 400 });
+  //   if (!waiverId) return new NextResponse("Missing waiverId", { status: 400 });
 
-//   const waiver = await prisma.waiver.findUnique({
-//     where: { id: waiverId },
-//     include: { signature: true },
-//   });
+  //   const waiver = await prisma.waiver.findUnique({
+  //     where: { id: waiverId },
+  //     include: { signature: true },
+  //   });
 
-//   if (!waiver || !waiver.signature)
-//     return new NextResponse("Waiver or signature not found", { status: 404 });
+  //   if (!waiver || !waiver.signature)
+  //     return new NextResponse("Waiver or signature not found", { status: 404 });
 
-//   const formattedDate = format(waiver.signature.date, "MMMM d, yyyy");
+  //   const formattedDate = format(waiver.signature.date, "MMMM d, yyyy");
 
-//   const pdfBuffer = await renderToBuffer(
-//     createElement(WaiverPDF, {
-//       name={waiver.signature.name}
-//       date={formattedDate}
-//       waiverId={waiver.signature.id}
-//       signatureUrl={`https://uploadthing.com/f/${waiver.signature.fileKey}`}
-//     })
-// );
+  //   const pdfBuffer = await renderToBuffer(
+  //     createElement(WaiverPDF, {
+  //       name={waiver.signature.name}
+  //       date={formattedDate}
+  //       waiverId={waiver.signature.id}
+  //       signatureUrl={`https://uploadthing.com/f/${waiver.signature.fileKey}`}
+  //     })
+  // );
 
-//   return new NextResponse(pdfBuffer, {
-//     status: 200,
-//     headers: {
-//       "Content-Type": "application/pdf",
-//       "Content-Disposition": `attachment; filename=waiver-${waiverId}.pdf`,
-//     },
-//   });
-// }
+  return NextResponse.json({ received: true });
+}
