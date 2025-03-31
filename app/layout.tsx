603f1d12a1e228060cc63cbfb8b802e57d6907d4
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2025-03-31 16:35:55 -0400

    minor fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import PostHogWrapper from "@/lib/posthog/posthogWrapper";
 import ClerkButtons from "@/components/landing/ClerkButtons";
 import { Suspense } from "react";
 
-
+export const dynamic = "force-dynamic"
 
 const inter = Inter({ subsets: ["latin"] });
 
